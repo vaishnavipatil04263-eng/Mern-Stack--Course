c8649b57afa9e25275ac2f9f5afe9a1d09528536
vaishnavi.patil <vaishnavipatil04263@gmail.com>
2026-03-04 01:40:26 +0530

    done with chenges

---
+++ 3 month march/CRUD Project/backend/sever.js
@@ -0,0 +1,13 @@
+// APILits
+
+//1.API create item-get data values from front end (items details) and store into Db
+//2. API update item-get item details from front end which item we need to update
+//3. API delet item-get ieem detels from front end andelete this record from database
+//4 API get all recordes -get all recoreds from DB and show to ui front end 
+
+const getdata =()=>{
+
+}
+function getdata(){
+    
+}
--- 3 month march/CRUD Project/frontend/vite-project/src/App.jsx
+++ 3 month march/CRUD Project/frontend/vite-project/src/App.jsx
@@ -4,130 +4,148 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
-
-import "./style.css"
+import { ToastContainer, toast } from 'react-toastify';
+import "./style.css";
+import { useState } from 'react';
 
 function App() {
-
-
+  const [Itemname, setitemname] = useState() //hook-use state hook
+  console.log(Itemname, "Item Name Value")
+  const handleOnChenge = (event) => {
+    setitemname(event.target.value)
+    console.log("typing on input field")
+  };
+   function SubmitFrom(e) {
+    e.preventDefault();
+    console.log("Form submitted")
+    toast.success(' Form Submitted', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <h2 className='text-danger text-center my-5'>CRUD - MERN Stack Project Start</h2>
       <div className='container'>
         <div className='row'>
           <div className='col-md-6'>
-            <h3 className='border text-center'>Add Item</h3>
+            <h3 className='border text-center'>Create Item</h3>
             <Form>
               <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-              </Row>
-
-              <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="1234 Main St" />
-              </Form.Group>
-
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridState">
-                  <Form.Label>State</Form.Label>
-                  <Form.Select defaultValue="Choose...">
-                    <option>Choose...</option>
-                    <option>...</option>
-                  </Form.Select>
+                  <Form.Label>Item name</Form.Label>
+                  <Form.Control type="Text" placeholder="Enter Item Name " onChange={() => handleOnChenge(event)}
+                  />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control />
+                  <Form.Label>Discriptions</Form.Label>
+                  <Form.Control type="text" placeholder=' Enter Discription' />
                 </Form.Group>
               </Row>
 
-              <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Purchase Price</Form.Label>
+                  <Form.Control type="Number" placeholder="Enter Purchase Price"
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridAddress1">
+                  <Form.Label>Selling Price</Form.Label>
+                  <Form.Control type="Number" placeholder="Enter selling price" />
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridCity">
+                  <Form.Label>Quantity</Form.Label>
+                  <Form.Control type="Numer" placeholder='Enter Quantity ' />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>Unit</Form.Label>
+                  <Form.Select defaultValue="Choose...">
+                    <option>Choose Unit</option>
+                    <option>Pice</option>
+                    <option>Box</option>
+                    <option>Kg</option>
+                    <option>Gram</option>
+                    <option>Litter</option>
+
+                  </Form.Select>
+                </Form.Group>
+              </Row>
               <div className='text-center'>
-                <Button variant="primary" type="submit" className='w-50'>
-                Submit
-              </Button>
+                <Button variant="primary" type="submit" className='w-50'
+                  onClick={SubmitFrom}
+                >
+                  submit
+                </Button>
               </div>
-              
+
             </Form>
           </div>
           <div className='col-md-6'>
-            <h3 className='border text-center'>Item List</h3>
+            <h3 className='border text-center'>Get Items</h3>
             <Table striped bordered hover>
               <thead>
                 <tr>
-                  <th>Email</th>
-                  <th>Password</th>
-                  <th>Address</th>
-                  <th>City</th>
-                  <th>State</th>
-                  <th>Zip</th>
+                  <th>Id</th>
+                  <th>Item Name</th>
+                  <th>Discriptions</th>
+                  <th>Purchase Price</th>
+                  <th>Selling Price</th>
+                  <th>Quantity</th>
+                  <th>Unit</th>
+                  <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>test@gmail.com</td>
-                  <td>1234</td>
-                  <td>Koregon</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415-105</td>
+                  <td>1</td>
+                  <td>Pen</td>
+                  <td>jel pen</td>
+                  <td>10</td>
+                  <td>20</td>
+                  <td>10</td>
+                  <td>Box</td>
+                  <td className='d-flex'>
+                    <button className='btn btn-success'> Edit</button>
+                    <button className='btn btn-danger mx-2'>Delete</button>
+                  </td>
                 </tr>
                 <tr>
-                  <td>test@gmail.com</td>
-                  <td>1234</td>
-                  <td>Koregon</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415-105</td>
+                  <td>2</td>
+                  <td>Book</td>
+                  <td>classment</td>
+                  <td>10</td>
+                  <td>20</td>
+                  <td>10</td>
+                  <td>Box</td>
+                  <td className='d-flex'>
+                    <button className='btn btn-success'> Edit</button>
+                    <button className='btn btn-danger mx-2'>Delete</button>
+                  </td>
                 </tr>
-               <tr>
-                  <td>test@gmail.com</td>
-                  <td>1234</td>
-                  <td>Koregon</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415-105</td>
-                </tr>
-                <tr>
-                  <td>test@gmail.com</td>
-                  <td>1234</td>
-                  <td>Koregon</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415-105</td>
-                </tr>
-                <tr>
-                  <td>test@gmail.com</td>
-                  <td>1234</td>
-                  <td>Koregon</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415-105</td>
-                </tr>
-               <tr>
-                  <td>test@gmail.com</td>
-                  <td>1234</td>
-                  <td>Koregon</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415-105</td>
-                </tr>             
+
               </tbody>
             </Table>
           </div>
