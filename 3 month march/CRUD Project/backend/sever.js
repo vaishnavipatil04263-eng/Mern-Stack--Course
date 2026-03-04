ab4545fef15b9f2e991cbe0ffcc01620b59510da
vaishnavi.patil <vaishnavipatil04263@gmail.com>
2026-03-04 19:38:37 +0530

    done chenges

--- 3 month march/CRUD Project/backend/sever.js
+++ 3 month march/CRUD Project/backend/sever.js
@@ -5,9 +5,28 @@
 //3. API delet item-get ieem detels from front end andelete this record from database
 //4 API get all recordes -get all recoreds from DB and show to ui front end 
 
-const getdata =()=>{
+// const getdata =()=>{
 
-}
-function getdata(){
+// }
+// function getdata(){
     
-}
+// }
+
+console.log("hello")
+
+const express = require("express")
+const { ServerDescription } = require("mongodb")
+const app = express()
+const mongoose = require("mongoose")
+
+
+app.use(express.json())
+mongoose.connect("mongodb://localhost:27017/item-database").then( () => console.log("mongo DB Connected")).catch((error)=> console.log(error))
+
+const itemsSchema = new mongoose.Schema({
+    name  :String,
+    Description : String,
+    sellingprice : Number
+})
+
+const items = new mongoose.model("Item", itemsSchema)
